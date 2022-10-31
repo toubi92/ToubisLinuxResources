950222098969331bca6fa74110928bcedecae1bd
toubi92 <42187428+toubi92@users.noreply.github.com>
2022-10-31 14:36:36 +0100

    That stuff didn't work on my last attempt...

--- setup/tmux/customize.sh
+++ setup/tmux/customize.sh
@@ -4,10 +4,6 @@
 locale=en_US.UTF-8
 sudo locale-gen $locale
 sudo update-locale LANG=$locale
-if [ "$?" -ne "0" ]
-then
-    #Failed to update locale. Probably need to generate
-    sudo sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-    sudo dpkg-reconfigure --frontend=noninteractive locales
-    sudo update-locale LANG=$locale
-fi
+sudo sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sudo dpkg-reconfigure --frontend=noninteractive locales
+sudo update-locale LANG=$locale
